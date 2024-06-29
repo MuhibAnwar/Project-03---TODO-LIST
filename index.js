bea8f841737c223c253e21d986bdbd6d08748f22
Muhib Anwar <160424220+MuhibAnwar@users.noreply.github.com>
2024-06-29 17:00:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+let todo = [];
+let condition = true;
+while (condition) {
+    // Prompt the user for the to-do item and whether they want to add more items
+    const input = await inquirer.prompt([
+        {
+            name: 'TODOLIST',
+            type: 'input',
+            message: 'What do you want to do?',
+        },
+        {
+            name: 'ADDMORE',
+            type: 'list',
+            choices: ['true', 'false'],
+            message: 'Do you want to add more?',
+        },
+    ]);
+    // Add the new to-do item to the list
+    todo.push(input.TODOLIST);
+    // Update the condition based on the user's input
+    condition = input.ADDMORE === 'true';
+}
+// Print the final to-do list
+console.log(todo);
